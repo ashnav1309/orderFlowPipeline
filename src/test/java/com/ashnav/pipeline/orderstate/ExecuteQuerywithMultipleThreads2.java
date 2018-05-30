ea304e34caf18ef36111710f965a106cbfcb4744
ashnav.saxena <>
2018-05-30 18:07:19 +0530

    Class changes

--- src/test/java/com/ashnav/pipeline/orderstate/ExecuteQuerywithMultipleThreads2.java
+++ src/test/java/com/ashnav/pipeline/orderstate/ExecuteQuerywithMultipleThreads2.java
@@ -7,10 +7,10 @@ import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import junit.framework.Assert;
 import macros.ConstantLiterals;
 import utilities.ConnectToGSheet;
 import utilities.ConnectToMysql;
